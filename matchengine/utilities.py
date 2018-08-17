bcb3b36040c20c98fc4aeabd182430a3a3cf92da
Kelsey Zhu <kzhu@uhnresearch.ca>
2018-08-17 14:51:29 -0400

    fixing match cancer type with 'and' tag

--- matchengine/utilities.py
+++ matchengine/utilities.py
@@ -472,15 +472,21 @@ def clean_query_for_msi(g):
 
 def get_match_cancer_type(trial_segment):
     try:
-        if 'or' not in trial_segment['match'][0]['and'][1]:
-            return trial_segment['match'][0]['and'][1]['clinical']['oncotree_primary_diagnosis']
-        else:
+        return trial_segment['match'][0]['and'][1]['clinical']['oncotree_primary_diagnosis']
+    except:
+        try:
             match_cancer_type_list = []
             for one_type in trial_segment['match'][0]['and'][1]['or']:
                 match_cancer_type_list.append(one_type['clinical']['oncotree_primary_diagnosis'])
-            return ','.join(match_cancer_type_list) if len(match_cancer_type_list) > 0 else ''
-    except:
-        return ''
+            return ' '.join(match_cancer_type_list) if len(match_cancer_type_list) > 0 else ''
+        except:
+            try:
+                match_cancer_type_list = []
+                for one_type in trial_segment['match'][0]['and'][1]['and']:
+                    match_cancer_type_list.append(one_type['clinical']['oncotree_primary_diagnosis'])
+                return ' '.join(match_cancer_type_list) if len(match_cancer_type_list) > 0 else ''
+            except:
+                return ''
 
 def get_cancer_type_match(trial):
     """
